a2960193f67d45e98246f9d562201dfcb5113c75
Addy771 <addy771@gmail.com>
2016-10-16 22:16:47 -0700

    Added pushbutton functionality
    uses bounce2 library

--- Garage_closer.ino
+++ Garage_closer.ino
@@ -4,10 +4,12 @@ This program uses an HC-SR04 ultrasonic distance sensor  to detect if a garage d
 If the door has been open for too long, a signal is sent to the garage door opener so it closes the door
 */
 
+#include <Bounce2.h>
 #include <FastLED.h>
 #define NUM_LEDS 10
-#define TIME_INCREMENT 30  // How much time each LED segment is worth in minutes
+#define TIME_INCREMENT 1  // How much time each LED segment is worth in minutes
 #define SEGMENT_BRIGHTNESS 50  // How bright an LED should be when fully on, on a scale of 0-255
+#define DEBOUNCE_TIME 20   // How many ms must pass before the button is considered stable
 
 #define SECS_PER_SEG TIME_INCREMENT * 60ul
 FASTLED_USING_NAMESPACE
@@ -17,12 +19,16 @@ FASTLED_USING_NAMESPACE
 const int SENS1_TRIG_PIN = 7;
 const int SENS1_ECHO_PIN = 8;
 const int LED_DATA_PIN = 2;
+const int BTN_PIN = 9;
 const int DOOR1_SIGNAL_PIN = 3;
 
 // Anything over 400 cm (23200 us pulse) is "out of range"
 const unsigned int MAX_DIST = 23200;
 
+Bounce button(BTN_PIN, DEBOUNCE_TIME);
 CRGB leds[NUM_LEDS];
+unsigned long int remaining_time = 300;
+unsigned long int ref_time;
 
 void draw_led_bar(unsigned long int time);
 
@@ -32,9 +38,11 @@ void setup() {
   pinMode(SENS1_TRIG_PIN, OUTPUT);
   digitalWrite(SENS1_TRIG_PIN, LOW);
   
+  pinMode(BTN_PIN, INPUT_PULLUP);
+  
   FastLED.addLeds<NEOPIXEL, LED_DATA_PIN>(leds, NUM_LEDS);
   
-  draw_led_bar(9050);
+  ref_time = millis();
   
   /************************************/
   // Init pushbutton/debounce library
@@ -42,7 +50,20 @@ void setup() {
 }
 
 void loop() {
+  button.update();
   
+  if (button.fell())
+    remaining_time += SECS_PER_SEG;
+  
+  // if 1 second has passed, update the remaining_time
+  if (millis() - ref_time >= 1000)
+  {
+    if (remaining_time > 0)
+      remaining_time--;
+    ref_time += 1000;
+  }
+  
+  draw_led_bar(remaining_time);
   // poll pushbutton
   
   // Test door1 open status
@@ -51,8 +72,7 @@ void loop() {
   
   // Process button input, add to timer if needed
   
-  // Convert time remaining to LED indication. Segments will be lit up, last segment fades down as time runs out
-  
+    
 
 
 }
